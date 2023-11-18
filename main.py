ce759c61ef5520950bfb0cee4bb8311fdb6e76ac
irisxvii <140954119+irisxvii@users.noreply.github.com>
2023-11-18 11:44:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load pre-trained emotion recognition model
+model = load_model('path/to/emotion_model.h5')
+
+# Define the emotion labels
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# Load the pre-trained face cascade for detecting faces
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Open the webcam (you can also use a video file instead)
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+    # Process each detected face
+    for (x, y, w, h) in faces:
+        # Extract the face region
+        face_roi = gray[y:y + h, x:x + w]
+
+        # Resize the face image to match the input size of the model
+        face_roi = cv2.resize(face_roi, (48, 48))
+        face_roi = np.expand_dims(face_roi, axis=-1)
+        face_roi = np.expand_dims(face_roi, axis=0)
+
+        # Predict the emotion
+        emotion_probabilities = model.predict(face_roi)
+        emotion_index = np.argmax(emotion_probabilities)
+        emotion = emotion_labels[emotion_index]
+
+        # Draw a rectangle around the detected face and display the predicted emotion
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame, f'Emotion: {emotion}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2, cv2.LINE_AA)
+
+    # Display the resulting frame
+    cv2.imshow('Emotion Recognition', frame)
+
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all windows
+cap.release()
+cv2.destroyAllWindows()
 
